69c5063a19fee437adbcbf84ed4f1dff9bb621b2
navfal2010 <navfal808@gmail.com>
2024-03-09 10:28:58 +0500

    done

---
+++ main.js
@@ -0,0 +1,8 @@
+let first = prompt('String typedagi malumotni kriting')
+let second = prompt('Number typedagi malumotni kriting')
+let third = prompt('Boolean typedagi malumotni kriting')
+alert('hamma malumotlarni togri kiritkaningizga ishonchingiz komilmi?')
+alert(`Siz kiritgan ${first} sozi string typega tegishli\nSiz kiritgan ${second} sozi number typega tegishli\nSiz kiritgan ${third} sozi boolean typega tegishli`)
+console.log('String: '+first);
+console.log('Number: '+second);
+console.log('Boolen: '+third);
